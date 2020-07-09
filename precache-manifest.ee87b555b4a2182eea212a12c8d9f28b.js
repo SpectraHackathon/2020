4e42acea5e6db8c37df4498f80a5939bdd4b8967
samkitkat <samarbarakat23@gmail.com>
2020-07-08 21:55:37 -0700

    Updates

---
+++ precache-manifest.ee87b555b4a2182eea212a12c8d9f28b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfd679cac5726e3be699a52caeb18b7",
+    "url": "/2020/index.html"
+  },
+  {
+    "revision": "cadcdf49c00d2e47af5a",
+    "url": "/2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87dd1dc2cd5c5f07b6d1",
+    "url": "/2020/static/js/2.f1675ec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020/static/js/2.f1675ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cadcdf49c00d2e47af5a",
+    "url": "/2020/static/js/main.07ca9ce1.chunk.js"
+  },
+  {
+    "revision": "1993597f2ff9f23ff976",
+    "url": "/2020/static/js/runtime-main.4e82c10b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
