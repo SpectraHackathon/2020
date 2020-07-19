ef93f878fac6642449e4351b591ebcbbee2bcce6
theangelaluo <theangelaluo@gmail.com>
2020-07-18 20:47:23 -0700

    added pst to schedule

--- src/Components/dayOneData.js
+++ src/Components/dayOneData.js
@@ -2,27 +2,27 @@
 const dayOneData = [
     {
         id: 1,
-        time: "9am",
+        time: "9am PST",
         name: "Registration and Team Formation"
     },
     {
         id: 3,
-        time: "10am",
+        time: "10am PST",
         name: "Opening Ceremony / Keynote Speaker"
     },
     {
         id: 5,
-        time: "10:30am",
+        time: "10:30am PST",
         name: "Hacking Begins"
     },
     {
         id: 7,
-        time: "10:30am - 9pm",
+        time: "10:30am - 9pm PST",
         name: "Mix of workshops, break activities, panels, networking"
     },
     {
         id: 9,
-        time: "9pm",
+        time: "9pm PST",
         name: "End of Day 1"
       },
 ]
--- src/Components/dayTwoData.js
+++ src/Components/dayTwoData.js
@@ -2,32 +2,32 @@
 const dayTwoData = [
     {
         id: 2,
-        time: "9am",
+        time: "9am PST",
         name: "Morning yoga and/or networking time"
     },
     {
         id: 4,
-        time: "10am",
+        time: "10am PST",
         name: "Day 2 of workshops begins"
     },
     {
         id: 6,
-        time: "10am - 2pm",
+        time: "10am - 2pm PST",
         name: "Mix of workshops, break activities, networking"
     },
     {
         id: 8,
-        time: "2pm",
+        time: "2pm PST",
         name: "Submission DUE on Devpost + Judging Begins"
     },
     {
         id: 10,
-        time: "5pm",
+        time: "5pm PST",
         name: "Closing Ceremony + Winners Announced"
     },
     {
       id: 11,
-      time: "5:30pm",
+      time: "5:30pm PST",
       name: "End of Event"
     }
 ]
